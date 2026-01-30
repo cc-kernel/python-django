8cb4344e1df57114f6af62957daae80cb92a77b9
ChrisC <cchristie035@gmail.com>
2026-01-29 23:10:31 -0500

    wip: add html and css on django

--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse("Welcome to MyWeb! Python and Django application.")
+
+
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to MyWeb! Python and Django application.")
+    return render(request, 'main/home.html')
